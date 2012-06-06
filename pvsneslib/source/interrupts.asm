051c435ef7c1fb44e6fad24799410eb1cde43b61
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2012-06-06 06:56:36 +0000

    initial version

---
+++ pvsneslib/source/interrupts.asm
@@ -0,0 +1,22 @@
+.section ".interrupts_text" superfree
+
+;---------------------------------------------------------------------------
+WaitForVBlank:
+	wai
+	rtl
+	
+	pha
+	php
+	sep	#$20
+-:
+	lda.l	REG_RDNMI
+	bmi	-
+-:
+	lda.l	REG_RDNMI
+	bpl	-
+	plp
+	pla
+	rtl
+
+
+.ends
---
+++ pvsneslib/source/videos.asm
@@ -0,0 +1,97 @@
+.equ REG_INIDISP	$2100
+.equ REG_MOSAIC		$2106
+
+.section ".pvsneslib_text" superfree
+
+;---------------------------------------------------------------------------
+setFadeEffect:
+	php
+;	rep	#$10
+;	sep	#$20
+	sep	#$30
+	
+	lda	5,s
+	tax
+	cpx #1	; FADE_OUT ?
+	beq _fadeouteffect
+
+	ldx	#0
+-:
+	jsr.w	_wait_nmi
+	txa
+	sta.l	REG_INIDISP
+	inx
+	cpx	#$10
+	bne	-
+	plp
+	rtl
+
+_fadeouteffect:
+	ldx	#15
+-:
+	jsr.w	_wait_nmi
+	txa
+	sta.l	REG_INIDISP
+	dex
+	bpl	-
+	plp
+	rtl
+
+;---------------------------------------------------------------------------
+setMosaicEffect:
+	php
+;	rep	#$10
+;	sep	#$20
+	sep	#$30
+	
+	lda	5,s
+	tax
+	cpx #1	; MOSAIC_OUT ?
+	beq _mosaicouteffect
+
+	ldx	#0
+-:
+	jsr.w	_wait_nmi
+	txa
+	asl	a
+	asl	a
+	asl	a
+	asl	a		; Mosaic size in d4-d7
+	ora	#1		; Enable effect for BG0
+	sta.l	REG_MOSAIC
+	inx
+	cpx	#$10
+	bne	-
+	plp
+	rtl
+
+;---------------------------------------------------------------------------
+_mosaicouteffect:
+	ldx	#14
+-:
+	jsr.w	_wait_nmi
+	txa
+	asl	a
+	asl	a
+	asl	a
+	asl	a		; Mosaic size in d4-d7
+	ora	#1		; Enable effect for BG0
+	sta.l	REG_MOSAIC
+	dex
+	bpl	-
+	plp
+	rtl
+
+_wait_nmi:
+	php
+	sep	#$20
+-:
+	lda.l	REG_RDNMI
+	bmi	-
+-:
+	lda.l	REG_RDNMI
+	bpl	-
+	plp
+	rts
+
+.ends
