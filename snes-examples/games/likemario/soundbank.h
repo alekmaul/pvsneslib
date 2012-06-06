160ed14a1b95d7eceea0deb0f45effe40c450131
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2012-06-06 07:06:56 +0000

    remove useless files

--- snes-examples/games/likemario/soundbank.h
+++
@@ -1,9 +0,0 @@
-// snesmod soundbank definitions
-
-#ifndef __SOUNDBANK_DEFINITIONS__
-#define __SOUNDBANK_DEFINITIONS__
-
-#define MOD_OVERWORLD                   	0
-
-
-#endif // __SOUNDBANK_DEFINITIONS__
