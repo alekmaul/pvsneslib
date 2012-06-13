3b42f7b569fdf626940547f9e130b87a211a224a
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2012-06-13 04:09:34 +0000

    change version

--- pvsneslib/include/snes/libversion.h
+++ pvsneslib/include/snes/libversion.h
@@ -3,8 +3,8 @@
 
 #define _PVSNESLIB_MAJOR_	1
 #define _PVSNESLIB_MINOR_	0
-#define _PVSNESLIB_PATCH_	0
+#define _PVSNESLIB_PATCH_	1
 
-#define _PVSNESLIB_STRING "libSNES Release 1.0.0"
+#define _PVSNESLIB_STRING "libSNES Release 1.0.1"
 
 #endif // __PVSNESLIBVERSION_H__
