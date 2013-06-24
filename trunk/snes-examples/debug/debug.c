637a6c798f925d9740c8dca2de90e18b28b9ca5e
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2013-06-24 15:01:47 +0000

    Initial commit

---
+++ trunk/snes-examples/debug/debug.c
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------
+
+
+	Simple timer demo
+	-- alekmaul
+
+
+---------------------------------------------------------------------------------*/
+#include <snes.h>
+
+char messtxt[16];
+
+//---------------------------------------------------------------------------------
+int main(void) {
+    // Initialize SNES 
+	consoleInit();
+    
+	// Draw a wonderfull text :P
+	consoleNocashMessage("JUST COUNT VBL");
+
+	// Now Put in 16 color mode and disable Bgs except current
+	setMode(BG_MODE1,0);  bgSetDisable(1);  bgSetDisable(2);
+
+	// Wait for nothing :P
+	setBrightness(0xF);  
+	
+	while(1) {
+		sprintf(messtxt,"FPS=%d\r\n",snes_vblank_count);
+		consoleNocashMessage(messtxt);
+		WaitForVBlank();
+	}
+	return 0;
+}
