84f0114ac8e01b2b549209d3d43caac2b8d69a6d
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2013-07-15 05:38:36 +0000

    Bug fix for mosaic and fading

--- trunk/pvsneslib/source/videos.asm
+++ trunk/pvsneslib/source/videos.asm
@@ -6,11 +6,13 @@
 ;---------------------------------------------------------------------------
 setFadeEffect:
 	php
-;	rep	#$10
-;	sep	#$20
+	
+	phx
+	phy
+
 	sep	#$30
 	
-	lda	5,s
+	lda	9,s
 	tax
 	cpx #1	; FADE_OUT ?
 	beq _fadeouteffect
@@ -23,6 +25,11 @@ setFadeEffect:
 	inx
 	cpx	#$10
 	bne	-
+	
+	rep #$30
+	
+	ply
+	plx
 	plp
 	rtl
 
@@ -34,17 +41,25 @@ _fadeouteffect:
 	sta.l	REG_INIDISP
 	dex
 	bpl	-
+	
+	rep #$30
+	
+	ply
+	plx
 	plp
 	rtl
 
 ;---------------------------------------------------------------------------
+; void setMosaicEffect(u8 mode, u8 bgNumbers)
 setMosaicEffect:
 	php
-;	rep	#$10
-;	sep	#$20
-	sep	#$30
+
+	phx
+	phy
 	
-	lda	5,s
+	sep	#$30
+
+	lda	9,s                                             ; mode
 	tax
 	cpx #1	; MOSAIC_OUT ?
 	beq _mosaicouteffect
@@ -52,46 +67,58 @@ setMosaicEffect:
 	ldx	#0
 -:
 	jsr.w	_wait_nmi
-	txa
-	asl	a
-	asl	a
-	asl	a
-	asl	a		; Mosaic size in d4-d7
-	ora	#3		; Enable effect for BG0/1
-	sta.l	REG_MOSAIC
-	inx
-	cpx	#$10
-	bne	-
-	plp
-	rtl
-
-;---------------------------------------------------------------------------
-_mosaicouteffect:
-	ldx	#14
--:
+	jsr.w	_wait_nmi
 	jsr.w	_wait_nmi
 	txa
 	asl	a
 	asl	a
 	asl	a
 	asl	a		; Mosaic size in d4-d7
-	ora	#1		; Enable effect for BG0
+	ora	#$3		; Enable effect for BG0/1
+	sta.l	REG_MOSAIC
+	inx
+	cpx	#$10
+	bne	-
+	
+	rep #$30
+	
+	ply
+	plx
+	plp
+	rtl
+
+;---------------------------------------------------------------------------
+_mosaicouteffect:
+	ldx	#14
+	
+-:	jsr.w	_wait_nmi
+	jsr.w	_wait_nmi
+	jsr.w	_wait_nmi
+	txa
+	asl	a
+	asl	a
+	asl	a
+	asl	a		; Mosaic size in d4-d7
+	ora	#$3		; Enable effect for BG0
+	nop			; if not here, wla add a brk instruction o_O ....
 	sta.l	REG_MOSAIC
 	dex
 	bpl	-
+	
+	rep #$30
+	
+	ply
+	plx
 	plp
 	rtl
 
 _wait_nmi:
-	php
-	sep	#$20
 -:
 	lda.l	REG_RDNMI
 	bmi	-
 -:
 	lda.l	REG_RDNMI
 	bpl	-
-	plp
 	rts
 
 .ends
