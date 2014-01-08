2eff25689cd6ada69323751972f7a012e591c084
jeanmichel.girard <jeanmichel.girard@24079fe3-8419-16c7-7e76-060c9abc0596>
2014-01-08 20:20:13 +0000

    Update header for new sram management

--- trunk/snes-examples/graphics/Backgrounds/Mode3/data.asm
+++ trunk/snes-examples/graphics/Backgrounds/Mode3/data.asm
@@ -2,21 +2,18 @@
 
 .section ".rodata2" superfree
 
-patterns:
-.incbin "pvsneslib.pic" skip 0 read 32768
+patterns: .incbin "pvsneslib.pic" skip 0 read 32768
 
 .ends
 
 .section ".rodata3" superfree
 
-patterns1:
-.incbin "pvsneslib.pic" skip 32768 read 6976
+patterns1: .incbin "pvsneslib.pic" skip 32768 read 6976
+patterns1_end:
 
-map:
-.incbin "pvsneslib.map"
+map: .incbin "pvsneslib.map"
 map_end:
 
-palette:
-.incbin "pvsneslib.pal"
+palette: .incbin "pvsneslib.pal"
 
 .ends
