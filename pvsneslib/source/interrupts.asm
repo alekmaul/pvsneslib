dc0cc2abc4715777d3c3c2f0b00ba7aa5fa854a4
Alekmaul <alekmaul@portabledev.com>
2017-05-20 07:46:04 +0100

    update interrupt functions to last one

--- pvsneslib/source/interrupts.asm
+++ pvsneslib/source/interrupts.asm
@@ -5,18 +5,18 @@ WaitForVBlank:
 	wai
 	rtl
 	
-	pha
-	php
-	sep	#$20
--:
-	lda.l	REG_RDNMI
-	bmi	-
--:
-	lda.l	REG_RDNMI
-	bpl	-
-	plp
-	pla
-	rtl
+;	pha
+;	php
+;	sep	#$20
+;-:
+;	lda.l	REG_RDNMI
+;	bmi	-
+;-:
+;	lda.l	REG_RDNMI
+;	bpl	-
+;	plp
+;	pla
+;	rtl
 
 
 .ends
