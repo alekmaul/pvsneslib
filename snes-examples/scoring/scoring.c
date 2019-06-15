e2393ce9f7c847f23483172ffeb50edc8089490a
Alekmaul <alekmaul@portabledev.com>
2019-06-15 09:29:01 +0100

    Add scoreCmp example

--- snes-examples/scoring/scoring.c
+++ snes-examples/scoring/scoring.c
@@ -10,7 +10,7 @@
 
 extern char snesfont;
 
-scoMemory scoretst;
+scoMemory scoretst,scoretst1;
 char sz[128];
 
 //---------------------------------------------------------------------------------
@@ -36,6 +36,24 @@ int main(void) {
 	scoreAdd(&scoretst,0x2710);
 	sprintf(sz,"score hi %04x score lo %04x",(u16) scoretst.scohi,(u16) scoretst.scolo);
 	consoleDrawText(2,10,sz);
+
+	// Compare two scores
+	scoretst.scohi=18;scoretst.scolo=900;
+	scoretst1.scohi=17;scoretst1.scolo=900;
+	if (scoreCmp(&scoretst,&scoretst1)==0)  consoleDrawText(2,12,"1 scores equals");
+	else if (scoreCmp(&scoretst,&scoretst1)==0xFF)  consoleDrawText(2,12,"1 scoretst higher");
+	else if (scoreCmp(&scoretst,&scoretst1)==1)  consoleDrawText(2,12,"1 scoretst lower");
+	
+	scoretst1.scohi=18;scoretst1.scolo=901;
+	if (scoreCmp(&scoretst,&scoretst1)==0)  consoleDrawText(2,13,"2 scores equals");
+	else if (scoreCmp(&scoretst,&scoretst1)==-1)  consoleDrawText(2,13,"2 scoretst higher");
+	else if (scoreCmp(&scoretst,&scoretst1)==1)  consoleDrawText(2,13,"2 scoretst lower");
+	
+	scoretst1.scohi=19;scoretst1.scolo=900;
+	if (scoreCmp(&scoretst,&scoretst)==0)  consoleDrawText(2,14,"3 scores equals");
+	else if (scoreCmp(&scoretst,&scoretst1)==-1)  consoleDrawText(2,14,"3 scoretst higher");
+	else if (scoreCmp(&scoretst,&scoretst1)==1)  consoleDrawText(2,14,"3 scoretst lower");
+
 
 	// Now Put in 16 color mode and disable Bgs except current
 	setMode(BG_MODE1,0);  bgSetDisable(1);  bgSetDisable(2);
