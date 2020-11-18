242f23d916facf9a7ce203058fde15310c32c77d
Alekmaul <alekmaul@portabledev.com>
2020-11-18 08:08:49 +0100

    Fix text which was old ...

--- snes-examples/hello_world/hello_world.c
+++ snes-examples/hello_world/hello_world.c
@@ -23,8 +23,8 @@ int main(void) {
 
 	// Draw a wonderfull text :P
 	consoleDrawText(10,10,"Hello World !");
-	consoleDrawText(7,14,"WELCOME TO LIBSNES");
-	consoleDrawText(3,18,"HTTP://WWW.PORTABLEDEV.COM");
+	consoleDrawText(6,14,"WELCOME TO PVSNESLIB");
+	consoleDrawText(3,18,"HTTPS://WWW.PORTABLEDEV.COM");
 
 	// Wait for nothing :P
 	setScreenOn();  
