73a98a44847513557219d1f4ba0938bdc7db559c
Alekmaul <alekmaul@portabledev.com>
2021-04-10 10:13:41 +0200

    fix section finding, add more flexible options and less display on screen

--- tools/constify/constify.cpp
+++ tools/constify/constify.cpp
@@ -1,8 +1,33 @@
+/*---------------------------------------------------------------------------------
+
+	Copyright (C) 2012-2021
+		Alekmaul 
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+	Moves const data from .data/.ram.data to .rodata
+    Based on Constify from Mic
+
+---------------------------------------------------------------------------------*/
 /*
 	Constify - A tool for the SNES-SDK
 	Mic, 2010
 
-	Moves const data from .data/.ram.data to .rodata
 */
 
 #include <stdio.h>
@@ -99,19 +124,28 @@ void check_for_const(string s)
 //////////////////////////////////////////////////////////////////////////////
 void PrintOptions(const char *str)
 {
-	printf("\n\nUsage: constify [options] cfile asmfile output ...");
+	fprintf(stdout,"\n\nUsage: constify.exe [options] cfile asmfile output...");
 	
 	if(str[0]!=0)
-		printf("\nThe [%s] parameter is not recognized.",str);
+		printf("\n\nconstify: error 'The [%s] parameter is not recognized'\n",str);
 	
-	printf("\n\nOptions are:");
-	printf("\n\n--- section options ---");
+	printf("\n\nSection options:");
 	printf("\n-s#name           Change default .rodata to .#name section");
-	printf("\n\n--- Misc options ---");
-	printf("\n-q                quiet mode");
+	printf("\n\nMisc options:");
+    printf("\n-h                Display this information");
+	printf("\n-q                Quiet mode");
+    printf("\n-v                Display constify version information");
 	printf("\n");
 	
-} //end of PrintOptions()
+} 
+
+//////////////////////////////////////////////////////////////////////////////
+void PrintVersion(void)
+{
+	printf("\n\nconstify.exe ("CONSTIFYDATE") version "CONSTIFYVERSION"");
+    printf("\nCopyright (c) 2013-2021 Alekmaul");
+    printf("\nBased on constify by Mic\n");
+}
 
 /// M A I N ////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
@@ -120,45 +154,52 @@ int main(int argc, char **argv)
 	string oneLine, sectName;
 	bool saveCode;
 	unsigned int i, j;
+    int k;
 	int varOffs, varSize = 0, varsMoved, bytesMoved, ch, currSection;
 	char cFilebase[256]="";
 	char asmFilebase[256]="";
 	char outFilebase[256]="";
 
-	// Show something to begin :)
-	if (quietmode == 0) {
-		printf("\n==============================");
-		printf("\n---constify v"CONSTIFYVERSION" "CONSTIFYDATE"---");
-		printf("\n------------------------------");
-		printf("\n(c) 2013-2020 Alekmaul ");
-		printf("\nBased on constify by Mic");
-		printf("\n==============================\n");
-	}
-
-	if (argc < 4)
-	{
-		printf("\nERROR: Not enough parameters.");
-		PrintOptions((char *) "");
-		return 1;
-	}
-
 	//parse the arguments
 	sectName.clear();
-	for(i=1;i<argc;i++)
+	for(k=1;k<argc;k++)
 	{
-		if(argv[i][0]=='-')
+		if(argv[k][0]=='-')
 		{
-			if(argv[i][1]=='q') //quiet mode
+			if(argv[k][1]=='v') // show version
+            {
+                PrintVersion();
+                exit( 0 );
+            }
+			else if(argv[k][1]=='h') // show help
+            {
+                PrintOptions((char *) "");
+                exit( 0 );
+            }
+			else if(argv[k][1]=='q') //quiet mode
 			{
 				quietmode=1;
 			}
-			else if(argv[i][1]=='s') //section name
+			else if(argv[k][1]=='s') //section name
 			{
-				sectName = &argv[i][2];
+				sectName = &argv[k][2];
+			}
+            else //invalid option
+			{
+				PrintOptions(argv[k]);
+				exit( 1 );
 			}
 		}
 		else
 		{
+            // check we have the corrent number of arguments
+            if (argc < 4)
+            {
+                printf("\nconstify: error 'Not enough parameters'");
+                PrintOptions((char *) "");
+                exit( 1 );
+            }
+
 			//its not an option flag, so it must be the filenames
 			if(cFilebase[0]!=0) // if already defined... check other file
 			{
@@ -166,40 +207,44 @@ int main(int argc, char **argv)
 				{
 					if(outFilebase[0]!=0) // if already defined... problem :/
 					{
-						PrintOptions(argv[i]);
+						PrintOptions(argv[k]);
 						return 1;
 					}
 					else
-						strcpy(outFilebase,argv[i]);
+						strcpy(outFilebase,argv[k]);
 				}
 				else
-					strcpy(asmFilebase,argv[i]);
+					strcpy(asmFilebase,argv[k]);
 			}
 			else
-				strcpy(cFilebase,argv[i]);
+				strcpy(cFilebase,argv[k]);
 		}
-	}
-	
+	} // for(k=1;k<argc;k++)
+    
 	// check filenames
 	if(cFilebase[0]==0)  {
-		printf("\nERROR: You must specify a c source filename.");
+		printf("\nconstify: error 'You must specify a c source filename'");
 		PrintOptions("");
-		return 1;
+		exit( 1 );
 	}
 	if(outFilebase[0]==0)  {
-		printf("\nERROR: You must specify an output source filename.");
+		printf("\nconstify: error 'You must specify an output source filename'");
 		PrintOptions("");
-		return 1;
+		exit( 1 );
 	}
 	if(asmFilebase[0]==0)  {
-		printf("\nERROR: You must specify an asm output source filename.");
+		printf("\nconstify: error 'You must specify an asm output source filename'");
 		PrintOptions("");
-		return 1;
+		exit( 1 );
 	}
 	
 	
 	// Find all const variables that and store them in constVars
 	cFile = fopen(cFilebase, "rb");
+    if ( cFile == NULL ) {
+        printf( "\nconstify: error 'Cannot open file %s'\n", cFilebase );
+        exit( 1 );
+    }
 	oneLine.clear();
 	while (1)
 	{
@@ -230,8 +275,16 @@ int main(int argc, char **argv)
 
 
 	asmFile = fopen(asmFilebase, "rb");
+    if ( asmFile == NULL ) {
+        printf( "\nconstify: error 'Cannot open file %s'\n", asmFilebase );
+        exit( 1 );
+    }
 	outFile = fopen(outFilebase, "wb");
-
+    if ( outFile == NULL ) {
+        printf( "\nconstify: error 'Cannot create file %s'\n", outFilebase );
+        exit( 1 );
+    }
+    
 	oneLine.clear();
 	saveCode = false;
 	currSection = -1;
@@ -243,29 +296,31 @@ int main(int argc, char **argv)
 			{
 				if (oneLine.length())
 				{
-					if (oneLine.find(".ramsection \"ram.data\"") != string::npos)
+					//if (oneLine.find(".RAMSECTION \"ram.data\"") != string::npos)
+                    if (oneLine.find("APPENDTO \"globram.data\"") != string::npos)
 					{
 						saveCode = true;
 						currSection = 0;
 						asmSections[currSection].push_back(oneLine);
 					}
-					else if (oneLine.find(".section \".data\"") != string::npos)
+					//else if (oneLine.find(".SECTION \".data\"") != string::npos)
+                    else if (oneLine.find("APPENDTO \"glob.data\"") != string::npos)
 					{
 						saveCode = true;
 						currSection = 1;
 						asmSections[currSection].push_back(oneLine);
 					}
-					else if (oneLine.find(".section \".rodata\"") != string::npos)
+					else if (oneLine.find(".SECTION \".rodata\"") != string::npos)
 					{
 						saveCode = true;
 						currSection = 2;
 						if (sectName.length()) {
-							asmSections[currSection].push_back(".section \"."+sectName+"\" superfree");
+							asmSections[currSection].push_back(".SECTION \"."+sectName+"\" SUPERFREE");
 						}
 						else
 							asmSections[currSection].push_back(oneLine);
 					}
-					else if (oneLine.find(".ends") != string::npos)
+					else if (oneLine.find(".ENDS") != string::npos)
 					{
 						saveCode = false;
 						if (currSection == 2) break;
@@ -371,7 +426,7 @@ int main(int argc, char **argv)
 		//printf("%s at offset %d\n", constVars[i].data(), varOffs);
 	}
 
-	printf("Done! Moved %d variables (%d bytes)\n", varsMoved, bytesMoved);
+	printf("constify: Done 'Moved %d variables (%d bytes)'\n", varsMoved, bytesMoved);
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < asmSections[i].size(); j++)
@@ -379,7 +434,7 @@ int main(int argc, char **argv)
 			fprintf(outFile, "%s\n", asmSections[i][j].data());
 		}
 	}
-	fputs(".ends\n\n", outFile);
+	fputs(".ENDS\n\n", outFile);
 
 	while ((ch = fgetc(asmFile)) != EOF)
 	{
