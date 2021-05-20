01f8b6c20267aac6c79f213b203e2288eac9eeb8
Alekmaul <alekmaul@portabledev.com>
2021-05-20 06:05:36 +0200

    update comment date

--- pvsneslib/source/backgrounds.asm
+++ pvsneslib/source/backgrounds.asm
@@ -1,6 +1,6 @@
 ;---------------------------------------------------------------------------------
 ;
-;	Copyright (C) 2013-2020
+;	Copyright (C) 2013-2021
 ;		Alekmaul 
 ;
 ;	This software is provided 'as-is', without any express or implied
