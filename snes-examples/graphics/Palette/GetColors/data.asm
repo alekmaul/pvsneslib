2ee8cb422c654a8ca97e25c3e00e72c793a73ca6
Alekmaul <alekmaul@portabledev.com>
2021-05-28 09:20:06 +0200

    Initial commit

---
+++ snes-examples/graphics/Palette/GetColors/data.asm
@@ -0,0 +1,19 @@
+.include "hdr.asm"
+
+.section ".rodata1" superfree
+
+snesfont:
+.incbin "pvsneslibfont.pic"
+
+patterns:
+.incbin "ortf.pic" 
+patterns_end:
+
+map:
+.incbin "ortf.map"
+map_end:
+
+palette:
+.incbin "ortf.pal"
+
+.ends
