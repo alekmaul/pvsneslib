d84fa064c7df04e6c6276d6cfcfbb221e3b4b1e8
Alekmaul <alekmaul@portabledev.com>
2021-06-13 10:39:24 +0200

    Initial commit

---
+++ snes-examples/objects/mapandobjects/koopatroopa.h
@@ -0,0 +1,7 @@
+#ifndef KOOPATROOPA_H
+#define KOOPATROOPA_H
+
+extern void koopatroopainit(u16 xp, u16 yp, u16 type, u16 minx, u16 maxx);
+extern void koopatroopaupdate(u8 idx);
+
+#endif
---
+++ snes-examples/objects/mapandobjects/mario.h
@@ -0,0 +1,7 @@
+#ifndef MARIOMAP_H
+#define MARIOMAP_H
+
+extern void marioinit(u16 xp, u16 yp, u16 type, u16 minx, u16 maxx);
+extern void marioupdate(u8 idx);
+
+#endif
