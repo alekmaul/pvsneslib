e4536b01e051dcd9def04c3984439aa9dc3ca39c
kobenairb <kobenairb@gmail.com>
2022-08-31 17:44:05 +0800

    Add dockerfile for fedora, debian and ubuntu latest version

---
+++ dockerrun.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+function f_usage {
+
+    echo "usage: ${0} options:<d|h>"
+    echo "-d: name of the linux distrubtion (fedora, ubuntu or debian)"
+    echo "-h: help, print this message"
+
+}
+
+function f_check_distro {
+
+    distro="${1}"
+
+    if [[ $(echo ${distro} | egrep -c 'fedora|debian|ubuntu') -ne 1 ]]; then
+
+        echo "Wrong distrution: ${distro}"
+
+        f_usage; exit 1
+    fi
+
+}
+
+function f_delete_container {
+
+    is_container=$(docker ps -q -a | wc -l)
+
+    if [[ ${is_container} -ne 0 ]]; then
+
+        echo ">> [DOCKER] cleaning ${is_container} containers(s)"
+
+        docker rm  $(docker ps -q -a)
+        
+        echo
+    fi
+
+}
+
+function f_delete_none_image {
+
+    is_none=$(docker image ls | grep -c none)
+
+    if [[ ${is_none} -ne 0 ]]; then
+
+        echo ">> [DOCKER] cleaning ${is_none} <none> image(s)"
+
+        docker image ls | \
+            grep none | \
+            awk '{print $3}' | \
+            xargs docker image rm --force > /dev/null 2>&1
+        
+        echo
+    fi
+
+}
+
+# --------------- #
+# M A I N
+# --------------- #
+
+clear
+
+unset -v distro
+
+no_args="true"
+
+while getopts d:h flag
+do
+    case "${flag}" in
+        d) distro=${OPTARG}; f_check_distro ${distro};;
+        h) f_usage;;
+        *) f_usage; exit 1;;
+    esac
+    no_args="false"
+done
+
+[[ "$no_args" == "true" ]] && (f_usage; exit 1)
+
+f_delete_container
+f_delete_none_image
+
+image="pvsneslib-${distro}-image"
+
+echo -n ">> [DOCKER] Building image ${image} "
+docker build \
+    -f docker/${distro}/Dockerfile \
+    -t ${image} . > $(pwd)/docker/${distro}/docker_build.log 2>&1
+echo -e "[PASS]\n" || (echo -e "[FAILED]\n"; exit 1)
+
+echo ">> [DOCKER] Running container ${image}..."
+docker run -ti --rm \
+    -w "$(pwd)" \
+    -v "$(pwd):$(pwd)" \
+    -v "/etc/group:/etc/group:ro" \
+    -u "$(id -u):$(id -g)" \
+    -e "PVSNESLIB_HOME=$(pwd)" \
+    -e "DISTRO=${distro}" \
+    ${image}
