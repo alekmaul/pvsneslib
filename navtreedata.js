435f8a02e2fc8d1c9b602051681019db82e05e3b
alekmaul <alekmaul@portabledev.com>
2022-11-12 05:50:54 +0000

    Deploying to gh-pages from  @ 06135180793f760929da870e2b614c1eacb37127 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -29,10 +29,10 @@ var NAVTREE =
       [ "Audio API", "index.html#audio_api", null ],
       [ "Memory", "index.html#memory_api", null ],
       [ "System", "index.html#system_api", null ],
-      [ "User Input/ouput", "index.html#user_io_api", null ],
+      [ "User Input/output", "index.html#user_io_api", null ],
       [ "Engine API functions", "index.html#engine_api", null ],
       [ "Miscellaneous functions", "index.html#misc_api", null ],
-      [ "Usefull links", "index.html#external_links", null ],
+      [ "Useful links", "index.html#external_links", null ],
       [ "Special Thanks", "index.html#special_thanks", null ]
     ] ],
     [ "Data Structures", "annotated.html", [
