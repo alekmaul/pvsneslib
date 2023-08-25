ef1f84c3925819ab22b87b88645f27201fc318dc
digifox (malik alayli) <digital.fox.contact@gmail.com>
2023-08-25 10:52:58 +0200

    fix: update program title

--- snes-examples/logo/snes-logo-capcom/hdr.asm
+++ snes-examples/logo/snes-logo-capcom/hdr.asm
@@ -15,7 +15,7 @@
 .SNESHEADER
   ID "SNES"                     ; 1-4 letter string, just leave it as "SNES"
 
-  NAME "LIBSNES TIMER SAMPLE "  ; Program Title - can't be over 21 bytes,
+  NAME "CAPCOM LOGO SAMPLE   "  ; Program Title - can't be over 21 bytes,
   ;    "123456789012345678901"  ; use spaces for unused bytes of the name.
 
   SLOWROM
