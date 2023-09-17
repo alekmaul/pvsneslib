d3651cd351c56f5ac8f3764ea63a4795fb22ef4d
Alekmaul <alekmaul@portabledev.com>
2023-09-17 10:29:50 +0200

    fix: add prelmiminary code

--- tools/gfx4snes/src/loadbmp.c
+++ tools/gfx4snes/src/loadbmp.c
@@ -26,4 +26,263 @@
   BMP BI_RLE8 compression support by Andrey Beletsky
 	
 ***************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "loadbmp.h"
+
+// This returns the description of a numerical error code in English. This is also
+//  the documentation of all the error codes.
+const char* bmp_error_text(unsigned code)
+{
+  switch(code)
+  {
+    		case 0: return "no error, everything went ok";
+    		case 1: return "nothing done yet"; 						// the Encoder/Decoder has done nothing yet, error checking makes no sense yet
+
+
+    case 10: return "end of input memory reached without huffman end code"; /*while huffman decoding*/
+    case 11: return "error in code tree made it jump outside of huffman tree"; /*while huffman decoding*/
+    case 13: return "problem while processing dynamic deflate block";
+    case 14: return "problem while processing dynamic deflate block";
+    case 15: return "problem while processing dynamic deflate block";
+    case 16: return "unexisting code while processing dynamic deflate block";
+    case 17: return "end of out buffer memory reached while inflating";
+    case 18: return "invalid distance code while inflating";
+    case 19: return "end of out buffer memory reached while inflating";
+    case 20: return "invalid deflate block BTYPE encountered while decoding";
+    case 21: return "NLEN is not ones complement of LEN in a deflate block";
+     /*end of out buffer memory reached while inflating:
+     This can happen if the inflated deflate data is longer than the amount of bytes required to fill up
+     all the pixels of the image, given the color depth and image dimensions. Something that doesn't
+     happen in a normal, well encoded, PNG image.*/
+    case 22: return "end of out buffer memory reached while inflating";
+    case 23: return "end of in buffer memory reached while inflating";
+    case 24: return "invalid FCHECK in zlib header";
+    		case 25: return "only BI_RLE8 compression method is supported";
+    case 26: return "FDICT encountered in zlib header while it's not used for PNG";
+    case 27: return "PNG file is smaller than a PNG header";
+    /*Checks the magic file header, the first 8 bytes of the PNG file*/
+    case 28: return "incorrect PNG signature, it's no PNG or corrupted";
+    case 29: return "first chunk is not the header chunk";
+    case 30: return "chunk length too large, chunk broken off at end of file";
+    case 31: return "illegal PNG color type or bpp";
+    case 32: return "illegal PNG compression method";
+    case 33: return "illegal PNG filter method";
+    case 34: return "illegal PNG interlace method";
+    case 35: return "chunk length of a chunk is too large or the chunk too small";
+    case 36: return "illegal PNG filter type encountered";
+    case 37: return "illegal bit depth for this color type given";
+    		case 38: return "the palette is not 16 or 256 colors"; 
+    case 39: return "more palette alpha values given in tRNS chunk than there are colors in the palette";
+    case 40: return "tRNS chunk has wrong size for greyscale image";
+    case 41: return "tRNS chunk has wrong size for RGB image";
+    case 42: return "tRNS chunk appeared while it was not allowed for this color type";
+    case 43: return "bKGD chunk has wrong size for palette image";
+    case 44: return "bKGD chunk has wrong size for greyscale image";
+    case 45: return "bKGD chunk has wrong size for RGB image";
+    case 48: return "empty input buffer given to decoder. Maybe caused by non-existing file?";
+    case 49: return "jumped past memory while generating dynamic huffman tree";
+    case 50: return "jumped past memory while generating dynamic huffman tree";
+    case 51: return "jumped past memory while inflating huffman block";
+    case 52: return "jumped past memory while inflating";
+    case 53: return "size of zlib data too small";
+    case 54: return "repeat symbol in tree while there was no value symbol yet";
+    /*jumped past tree while generating huffman tree, this could be when the
+    tree will have more leaves than symbols after generating it out of the
+    given lenghts. They call this an oversubscribed dynamic bit lengths tree in zlib.*/
+    case 55: return "jumped past tree while generating huffman tree";
+    case 56: return "given output image colortype or bitdepth not supported for color conversion";
+    case 57: return "invalid CRC encountered (checking CRC can be disabled)";
+    case 58: return "invalid ADLER32 encountered (checking ADLER32 can be disabled)";
+    case 59: return "requested color conversion not supported";
+    case 60: return "invalid window size given in the settings of the encoder (must be 0-32768)";
+    case 61: return "invalid BTYPE given in the settings of the encoder (only 0, 1 and 2 are allowed)";
+    /*LodePNG leaves the choice of RGB to greyscale conversion formula to the user.*/
+    case 62: return "conversion from color to greyscale not supported";
+    case 63: return "length of a chunk too long, max allowed for PNG is 2147483647 bytes per chunk"; /*(2^31-1)*/
+    /*this would result in the inability of a deflated block to ever contain an end code. It must be at least 1.*/
+    case 64: return "the length of the END symbol 256 in the Huffman tree is 0";
+    case 66: return "the length of a text chunk keyword given to the encoder is longer than the maximum of 79 bytes";
+    case 67: return "the length of a text chunk keyword given to the encoder is smaller than the minimum of 1 byte";
+    case 68: return "tried to encode a PLTE chunk with a palette that has less than 1 or more than 256 colors";
+    case 69: return "unknown chunk type with 'critical' flag encountered by the decoder";
+    case 71: return "unexisting interlace mode given to encoder (must be 0 or 1)";
+    case 72: return "while decoding, unexisting compression method encountering in zTXt or iTXt chunk (it must be 0)";
+    case 73: return "invalid tIME chunk size";
+    case 74: return "invalid pHYs chunk size";
+    /*length could be wrong, or data chopped off*/
+    case 75: return "no null termination char found while decoding text chunk";
+    case 76: return "iTXt chunk too short to contain required bytes";
+    case 77: return "integer overflow in buffer size";
+			case 78: return "failed to open file for reading"; /*file doesn't exist or couldn't be opened for reading*/
+	case 79: return "failed to open file for writing";
+    case 80: return "tried creating a tree of 0 symbols";
+    case 81: return "lazy matching at pos 0 is impossible";
+    case 82: return "color conversion to palette requested while a color isn't in palette";
+    		case 83: return "memory allocation failed";
+    case 84: return "given image too small to contain all pixels to be encoded";
+    case 86: return "impossible offset in lz77 encoding (internal bug)";
+    case 87: return "must provide custom zlib function pointer if LODEPNG_COMPILE_ZLIB is not defined";
+    case 88: return "invalid filter strategy given for LodePNGEncoderSettings.filter_strategy";
+    case 89: return "text chunk keyword too short or long: must have size 1-79";
+    /*the windowsize in the LodePNGCompressSettings. Requiring POT(==> & instead of %) makes encoding 12% faster.*/
+    case 90: return "windowsize must be a power of two";
+    case 91: return "invalid decompressed idat size";
+    case 92: return "too many pixels, not supported";
+    case 93: return "zero width or height is invalid";
+    case 94: return "header chunk must have a size of 13 bytes";
+
+
+
+
+  }
+  return "unknown error code";
+}
+
+// returns negative value on error. This should be pure C compatible, so no fstat. 
+static long bmp_filesize(const char* filename)
+{
+  FILE* file;
+  long size;
+  file = fopen(filename, "rb");
+  if(!file) return -1;
+
+  if(fseek(file, 0, SEEK_END) != 0)
+  {
+    fclose(file);
+    return -1;
+  }
+
+  size = ftell(file);
+  
+  // It may give LONG_MAX as directory size, this is invalid for us. 
+  if(size == LONG_MAX) size = -1;
+
+  fclose(file);
+  return size;
+}
+
+// load file into buffer that already has the correct allocated size. Returns error code.
+static unsigned bmp_buffer_file(unsigned char* out, size_t size, const char* filename)
+{
+  FILE* file;
+  size_t readsize;
+  file = fopen(filename, "rb");
+  if(!file) return 78;
+
+  readsize = fread(out, 1, size, file);
+  fclose(file);
+
+  if (readsize != size) return 78;
+  return 0;
+}
+
+// load file into buffer and allocate the correct size. Returns error code.
+unsigned bmp_load_file(unsigned char** out, size_t* outsize, const char* filename)
+{
+  long size = bmp_filesize(filename);
+
+  if (size < 0) return 78;
+  *outsize = (size_t)size;
+
+  *out = (unsigned char*) malloc((size_t)size);
+  if(!(*out) && size > 0) return 83; 						// the above malloc failed
+
+  return bmp_buffer_file(*out, (size_t)size, filename);
+}
+
+// Converts BMP data in memory to raw pixel data.
+unsigned bmp_decode(unsigned char** out, BMPState* state, unsigned* w, unsigned* h, const unsigned char* in, size_t insize)
+{
+    bmp_header bmphead;
+    bmp_info_header bmpinfohead;
+	unsigned state_error=0;
+	unsigned currentseek;
+
+	// get header
+	memcpy(&bmpinfohead,in, sizeof(bmpinfohead));
+
+	// not a valid BMP file: only 16, 256 colors
+    if (bmpinfohead.biBitCount != 4 || bmpinfohead.biBitCount != 8 )
+    {
+		state_error=38;
+    }
+	// not a valid BMP file: compressed and not BI_RLE8 supported
+    else if (bmpinfohead.biCompression != 0 && bmpinfohead.biCompression != BI_RLE8)
+    {
+		state_error=25;
+    }
+	// hearder seems valid
+	else {
+		// goto palette
+		currentseek=sizeof(bmp_header) + bmpinfohead.biSize;
+
+/*
+     fseek(fp, sizeof(bmp_header) + bmpinfohead.biSize, 0);
+	
+	// initially clear the palette if there are less then 256 colors in the file
+    memset(image->palette, 0, (size_t)(256 * sizeof(RGB_color)));
+
+    // read the palette information
+    for (index = 0; index < 256; index++)
+    {
+        image->palette[index].blue = getc(fp) >> 2;
+        image->palette[index].green = getc(fp) >> 2;
+        image->palette[index].red = getc(fp) >> 2;
+        // data=getc(fp);
+        getc(fp);
+    }
+
+    header = &image->header;
+    header->width = bmpinfohead.biWidth;
+    header->height = bmpinfohead.biHeight;
+*/
+
+	}
+
+	return state_error;
+#if 0
+  *out = 0;
+  decodeGeneric(out, w, h, state, in, insize);
+  if(state->error) return state->error;
+  if(!state->decoder.color_convert || lodepng_color_mode_equal(&state->info_raw, &state->info_png.color))
+  {
+    /*same color type, no copying or converting of data needed*/
+    /*store the info_png color settings on the info_raw so that the info_raw still reflects what colortype
+    the raw image has to the end user*/
+    if(!state->decoder.color_convert)
+    {
+      state->error = lodepng_color_mode_copy(&state->info_raw, &state->info_png.color);
+      if(state->error) return state->error;
+    }
+  }
+  else
+  {
+    /*color conversion needed; sort of copy of the data*/
+    unsigned char* data = *out;
+    size_t outsize;
+
+    /*TODO: check if this works according to the statement in the documentation: "The converter can convert
+    from greyscale input color type, to 8-bit greyscale or greyscale with alpha"*/
+    if(!(state->info_raw.colortype == LCT_RGB || state->info_raw.colortype == LCT_RGBA)
+       && !(state->info_raw.bitdepth == 8))
+    {
+      return 56; /*unsupported color mode conversion*/
+    }
+
+    outsize = lodepng_get_raw_size(*w, *h, &state->info_raw);
+    *out = (unsigned char*)lodepng_malloc(outsize);
+    if(!(*out))
+    {
+      state->error = 83; /*alloc fail*/
+    }
+    else state->error = lodepng_convert(*out, data, &state->info_raw,
+                                        &state->info_png.color, *w, *h);
+    lodepng_free(data);
+  }
+  return state->error;
+#endif  
+}
+
--- tools/gfx4snes/src/loadbmp.h
+++ tools/gfx4snes/src/loadbmp.h
@@ -1,4 +1,66 @@
 #ifndef _GFX4SNES_LOADBMP_H
 #define _GFX4SNES_LOADBMP_H
 
+ typedef  enum
+ {
+   BI_RGB = 0x0000,
+   BI_RLE8 = 0x0001,
+   BI_RLE4 = 0x0002,
+   BI_BITFIELDS = 0x0003,
+   BI_JPEG = 0x0004,
+   BI_PNG = 0x0005,
+   BI_CMYK = 0x000B,
+   BI_CMYKRLE8 = 0x000C,
+   BI_CMYKRLE4 = 0x000D
+ } Compression;
+
+typedef struct bmp_header_typ /**** BMP file header structure ****/
+{
+    unsigned short bfType;      /* Magic number for file */
+    unsigned int bfSize;        /* Size of file */
+    unsigned short bfReserved1; /* Reserved */
+    unsigned short bfReserved2; /* ... */
+    unsigned int bfOffBits;     /* Offset to bitmap data */
+} bmp_header;
+
+#define BF_TYPE 0x4D42 /* "MB" */
+
+typedef struct bmp_info_header_typ
+{
+    unsigned int biSize;         /* Size of info header */
+    int biWidth;                 /* Width of image */
+    int biHeight;                /* Height of image */
+    unsigned short biPlanes;     /* Number of color planes */
+    unsigned short biBitCount;   /* Number of bits per pixel */
+    unsigned int biCompression;  /* Type of compression to use */
+    unsigned int biSizeImage;    /* Size of image data */
+    int biXPelsPerMeter;         /* X pixels per meter */
+    int biYPelsPerMeter;         /* Y pixels per meter */
+    unsigned int biClrUsed;      /* Number of colors used */
+    unsigned int biClrImportant; /* Number of important colors */
+} bmp_info_header;
+
+typedef struct BMPcolor_typ
+{
+    unsigned char red;                                              // Red component if color 0-63
+    unsigned char green;                                            // Green component of color 0-63
+    unsigned char blue;                                             // Blue component of color 0-63
+} BMPcolor;
+
+typedef struct BMPInfo_typ
+{
+    unsigned compression_method;    // compression method of the original file. Always 0 or BI_RLE8.
+    BMPcolor palette[256];          // a maximum of 256 colors
+} BMPInfo;
+
+/*The settings, state and information for extended encoding and decoding.*/
+typedef struct BMPState_typ
+{
+  BMPInfo info_png;                         // info of the PNG image obtained after decoding
+} BMPState;
+
+extern const char* bmp_error_text(unsigned code);
+extern unsigned bmp_load_file(unsigned char** out, size_t* outsize, const char* filename);
+extern unsigned bmp_decode(unsigned char** out, BMPState* state, unsigned* w, unsigned* h, const unsigned char* in, size_t insize);
+
 #endif
