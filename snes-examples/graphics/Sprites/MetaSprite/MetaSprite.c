86509c9d4bc2d2f54ae970da55160379c970a411
Alekmaul <alekmaul@portabledev.com>
2025-12-28 06:41:25 +0100

    fix(*): change orientation of 32 pix sprite

--- snes-examples/graphics/Sprites/MetaSprite/MetaSprite.c
+++ snes-examples/graphics/Sprites/MetaSprite/MetaSprite.c
@@ -33,7 +33,7 @@ int main(void)
     setPalette(&spritehero32_pal, 128 + 0 * 16, 16 * 2);
     dmaCopyVram((u8 *) &spritehero32_til,0x0000, (&spritehero32_tilend - &spritehero32_til));
     dmaCopyVram((u8 *) &spritehero16_til,0x1000, (&spritehero16_tilend - &spritehero16_til));
-    dmaCopyVram((u8 *) &spritehero8_til,0x1600, (&spritehero8_tilend - &spritehero8_til));
+    // if size 8 enable dmaCopyVram((u8 *) &spritehero8_til,0x1600, (&spritehero8_tilend - &spritehero8_til));
 
     // Now Put in 16 color mode and disable other BGs except 1st one
     setMode(BG_MODE1, 0);
@@ -44,7 +44,7 @@ int main(void)
     // draw the sprite
     oamMetaDraw32(0, 16,140, (u8 *) spritehero32_metasprites[0],0x0000);
     oamMetaDraw16(8, 144,140, (u8 *) spritehero16_metasprites[0],OBJ_SMALL,0x0000);
-    oamMetaDraw8(16, 200,140, (u8 *) spritehero8_metasprites[0],0x0600);
+    //if size 8 enable oamMetaDraw8(16, 200,140, (u8 *) spritehero8_metasprites[0],0x0600);
 
     // Wait for nothing :P
     while (1)
