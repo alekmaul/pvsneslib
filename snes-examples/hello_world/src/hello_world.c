7f2a72bf17f0f39ade4ce3db7739a0dabc11dfeb
Alekmaul <alekmaul@portabledev.com>
2026-07-24 15:51:38 +0200

    fix(*): fix typo

--- snes-examples/hello_world/src/hello_world.c
+++ snes-examples/hello_world/src/hello_world.c
@@ -10,19 +10,17 @@
 int main(void)
 {
     // Initialize text console with our font
-    consoleSetTextMapPtr(0x6800);
-    consoleSetTextGfxPtr(0x3000);
-    consoleSetTextOffset(0x0100);
+    // Default Map is 0x6800, Gfx is 0x3000 and offset is 0
     consoleInitText(0, 16 * 2, &pvsneslibfont_til, &pvsneslibfont_pal);
 
     // Init background
-    bgSetGfxPtr(0, 0x2000);
+    bgSetGfxPtr(0, 0x3000);
     bgSetMapPtr(0, 0x6800, SC_32x32);
 
     // Now Put in 16 color mode and disable Bgs except current
     setMode(BG_MODE1, 0);
     bgSetDisable(1);
-    bgSetDisable(2);
+        bgSetDisable(2);
 
     // Draw a wonderful text :P
     consoleDrawText(10, 10, "Hello World !");
