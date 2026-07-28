882318cc56686329798b72088d37db2c202be84d
Alekmaul <alekmaul@portabledev.com>
2026-07-28 06:04:49 +0200

    chore(*): refactor example

--- snes-examples/systems/typeconsole/src/pal_ntsc.c
+++ snes-examples/systems/typeconsole/src/pal_ntsc.c
@@ -8,19 +8,14 @@
 ---------------------------------------------------------------------------------*/
 #include <snes.h>
 
-extern char snesfont, snespal;
-
 //---------------------------------------------------------------------------------
 int main(void)
 {
     // Initialize text console with our font
-    consoleSetTextMapPtr(0x6800);
-    consoleSetTextGfxPtr(0x3000);
-    consoleSetTextOffset(0x0100);
-    consoleInitText(0, 16 * 2, &snesfont, &snespal);
+    consoleInitDefaultText(0);
 
     // Init background
-    bgSetGfxPtr(0, 0x2000);
+    bgSetGfxPtr(0, 0x3000);
     bgSetMapPtr(0, 0x6800, SC_32x32);
 
     // Now Put in 16 color mode and disable Bgs except current
